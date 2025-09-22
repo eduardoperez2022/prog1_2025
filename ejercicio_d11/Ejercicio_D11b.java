a1b33048062ad2b1017435bacaf96eba848a04d8
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2025-09-22 20:11:04 -0300

    Ejercicio sobre lectura de archivos

---
+++ ejercicio_d11/Ejercicio_D11b.java
@@ -0,0 +1,13 @@
+package ejercicio_d11;
+
+public class Ejercicio_D11b {
+
+    public static void main(String[] args) {
+        
+        ejercicio eje = new ejercicio();
+        eje.lectura_3();
+        
+        
+    }
+    
+}
