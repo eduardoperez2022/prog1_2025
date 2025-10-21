8b8f7efc87aee785d82b43ab00a39cb06ec4788c
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2025-10-21 11:12:34 -0300

    Ejemplo uso interface

---
+++ ejercicio_603_otro/E603B.java
@@ -0,0 +1,11 @@
+package e603;
+ 
+public class E603B {
+ 
+    public static void main(String[] args) {
+        Juego juego = new Juego();
+        juego.iniciar();
+        juego.jugar();
+        juego.finalizar();
+    }
+}
