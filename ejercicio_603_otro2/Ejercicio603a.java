32c480b94d3133a37ecd20cb7cc6fbaaf3228928
eduardoperez2022 <102707420+eduardoperez2022@users.noreply.github.com>
2025-10-21 11:12:56 -0300

    Ejemplo uso interface

---
+++ ejercicio_603_otro2/Ejercicio603a.java
@@ -0,0 +1,11 @@
+package ejercicio603a;
+ 
+public class Ejercicio603a {
+ 
+    public static void main(String[] args) {
+        IJuego juego = new JuegoDado();
+        juego.iniciar();
+        juego.jugar();
+        juego.finalizar();
+    }
+}
